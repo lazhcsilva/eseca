f99dfa7b87be8c662c119d9fc824a2ace7afd6ea
Lázaro Henrique Silva <lazhcsilva@gmail.com>
2021-10-15 13:37:27 -0300

    Criando prototipo do projeto

---
+++ src/main/java/com/buymore/application/EsecaApplication.java
@@ -0,0 +1,13 @@
+package com.buymore.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buymore/application/EsecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buymore.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
